527dbbbe43c06d17f00597fdac2840f159513072
Alex Engelhart <asengelhart@yahoo.com>
2019-12-12 19:40:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -36,7 +36,7 @@ def prime?(num)
     if x != 1 && x != num - 1 #otherwise, skip to next a 
       (r - 1).times do
         x = (x * x) % num 
-        
+        if x ==
       end 
     end 
   end 
